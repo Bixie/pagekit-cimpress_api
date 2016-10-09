9bf8ee0f76dc77fb5677c9a654c2ea351563eb70
Matthijs Alles <admin@bixie.nl>
2016-10-09 16:48:56 +0200

    delivery options

--- src/Event/CartListener.php
+++ src/Event/CartListener.php
@@ -26,26 +26,33 @@ class CartListener implements EventSubscriberInterface {
 
     /**
      * @param DeliveryOptionsEvent $event
+     * @throws CartException
      */
     public function onDeliveryOptions ($event) {
         try {
             $cartHandler = $event->getCartHandler();
-            if ($delivery_address = $cartHandler->getDeliveryAddress()
-                and !!($delivery_address->address1 && $delivery_address->zipcode && $delivery_address->city && $delivery_address->country_code)) {
-                $delivery_options = $this->cimpress->getDeliveryOptions($cartHandler->getItems(), $delivery_address);
+            $delivery_options = $event->getDeliveryOptions();
+            $delivery_address = $cartHandler->getDeliveryAddress();
 
-                $delivery_options = array_map(function ($delivery_option) {
-                    return (new DeliveryOption([
-                        'id' => $delivery_option['DeliveryOptionId'],
-                        'business_days' => $delivery_option['BusinessDays'],
-                        'price' => $this->cimpress->getPrice($delivery_option['Price']['Amount'], 'delivery'),
-                        'currency' => $delivery_option['Price']['Currency'],
-                        'eta_date' => $delivery_option['DeliveryArriveByDate']
-                    ]));
-                }, $delivery_options);
-
-                $event->setDeliveryOptions($delivery_options);
+            $cimpress_items = array_filter($cartHandler->getItems(), function ($cartItem) {
+                return $cartItem->get('supplier') == 'cimpress';
+            });
+            if (!count($cimpress_items)) {
+                return;
             }
+            $cimpress_options = $this->cimpress->getDeliveryOptions($cimpress_items, $delivery_address);
+
+            $delivery_options['cimpress'] = array_map(function ($delivery_option) {
+                return (new DeliveryOption([
+                    'id' => $delivery_option['DeliveryOptionId'],
+                    'business_days' => $delivery_option['BusinessDays'],
+                    'price' => $this->cimpress->getPrice($delivery_option['Price']['Amount'], 'delivery'),
+                    'currency' => $delivery_option['Price']['Currency'],
+                    'eta_date' => $delivery_option['DeliveryArriveByDate']
+                ]));
+            }, $cimpress_options);
+
+            $event->setDeliveryOptions($delivery_options);
 
         } catch (CimpressApiException $e) {
             throw new CartException(sprintf('Error in Cimpress api: %s', $e->getMessage()), $e->getCode(), $e);
@@ -57,7 +64,7 @@ class CartListener implements EventSubscriberInterface {
      */
     public function subscribe () {
         return [
-            'bixie.cart.delivery_options' => 'onDeliveryOptions'
+            'bixie.game2art.delivery_options' => 'onDeliveryOptions'
         ];
     }
 }
